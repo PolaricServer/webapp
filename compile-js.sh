e2d7d79f1e160fb4e176de720672b4a6041253fa
oivindh <oivindh@fc1e02e6-8356-493b-8978-cc4a2141a8d3>
2012-03-17 23:46:40 +0000

    touchandler, iscroll

--- compile-js.sh
+++ compile-js.sh
@@ -7,12 +7,12 @@
 PATH=$PATH:/home/oivindh/bin
 
 cd KaMap
-ccompile --js DHTMLapi.js --js xhr.js --js kaMap.js --js kaTool.js --js kaQuery.js --js kaMouseTracker.js --js scalebar/scalebar.js > kamap-core.js
+ccompile --js DHTMLapi.js --js xhr.js --js touchHandler.js --js kaMap.js --js kaTool.js --js kaQuery.js --js kaMouseTracker.js --js scalebar/scalebar.js > kamap-core.js
 cd ..
 cd XMLOverlay
 ccompile --js kaXmlOverlay.js > compiled.js
 cd ..
 cd Aprs
-ccompile --js gpx.js --js popup.js --js menu.js --js jscoord.js --js startUp.js > compiled.js
+ccompile --js gpx.js --js iscroll.js --js popup.js --js menu.js --js jscoord.js --js startUp.js > compiled.js
 cd ..
 
