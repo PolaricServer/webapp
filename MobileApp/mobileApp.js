8b28c46dfe3f898a6b696f32ae42f2f9b729085d
oivindh <oivindh@fc1e02e6-8356-493b-8978-cc4a2141a8d3>
2012-07-29 00:17:53 +0000

    Android powermanagement

--- MobileApp/mobileApp.js
+++ MobileApp/mobileApp.js
@@ -1,5 +1,25 @@
- 
- 
+
+var powerMgmt_locked = false; 
+
+/* This function must be called after the initialisation of the uid and 
+ * the storage object 
+ */
+function powerMgmt_init() {
+    if ( storage['powermgmt_lock'] == 'true')
+       powerMgmt_lock(); 
+}
+
+function powerMgmt_lock() {
+  cordova.require('cordova/plugin/powermanagement').acquire(
+        function(x) { powerMgmt_locked = true; storage['powermgmt_lock'] = 'true'; }, 
+        function(x) { alert("Error: cannot acquire wake lock"); } ); 
+}
+
+function powerMgmt_unlock() {
+  cordova.require('cordova/plugin/powermanagement').release(
+        function(x) { powerMgmt_locked = false; storage['powermgmt_lock'] = 'false';}, 
+        function(x) { alert("Error: cannot release wake lock"); } ); 
+}
  
  function setStatus(txt) {
      geopos.innerHTML = txt;
