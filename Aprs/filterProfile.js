429d88576c401a70e544cb0127a710f639f67234
ohanssen <ohanssen@acm.org>
2014-08-19 18:51:29 +0200

    Fix problem with authorization

--- Aprs/filterProfile.js
+++ Aprs/filterProfile.js
@@ -10,10 +10,8 @@
  */
  
 function FilterProfile() {
-   this.selected = defaultFilterView;
    this.sFilter = storage[uid+'.filter'];  
    this.initialized = false; 
-   this.authorized = false; 
    this.done = false; 
    this.filterMenu = null;
    this.init();
@@ -21,28 +19,28 @@ function FilterProfile() {
  
  
 FilterProfile.prototype.selectedProf = function() {
-    return this.selected; 
+    return this.sFilter; 
 }
  
  
 FilterProfile.prototype.selectProfile = function(pname) {
    var t = this; 
-   if (pname == this.selected)
-      return;
-   this.selected = pname; 
-   if (t.done) {
-      storage[uid+'.filter'] = pname;
-      myOverlay.removePoint();
-      getXmlData(false);
+   if (pname != t.sFilter || !t.done) {
+      t.sFilter = pname; 
+      if (t.done) {
+         storage[uid+'.filter'] = pname;
+         myOverlay.removePoint();
+         getXmlData(false);
+      }
+      t.updateMenu(); 
    }
-   t.updateMenu(); 
 }
 
 
 
 FilterProfile.prototype.updateMenu = function() {
    for(var i in filterViews) {
-      if (filterViews[i].name == this.selected) {
+      if (filterViews[i].name == this.sFilter) {
          $('div#filterChoice').html(filterViews[i].title);
          break;
       }
@@ -53,23 +51,22 @@ FilterProfile.prototype.updateMenu = function() {
  
 FilterProfile.prototype.init = function() {
     var t = this;
-    var auth = isLoggedIn();
-    if (t.done && auth == t.authorized)
+    if (t.done)
         return;
         
-    t.authorized = auth; 
     var flt = args['view'];
     if (flt)
        t.sFilter = flt;  
     if (t.sFilter==null)
        t.sFilter = defaultFilterView;
-        
+
      if (!t.done) ctxtMenu.addCallback('FILTERS', function (m) 
      {
         for(var i in filterViews) {
            var fv = filterViews[i];
-           if (fv.name && fv.name.length > 1 && (!fv.restricted || auth)) 
+           if (fv.name && fv.name.length > 1 && (!fv.restricted || isLoggedIn())) 
               m.add(fv.title, function(x) { t.selectProfile(x); }, fv.name);
+           
         }
       });
       addContextMenu('filterMenu', 'FILTERS');
